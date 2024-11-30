27242fd11cd06b01bf72fd7abed01855251dd57e
Okasha Habib <okashabib.qsols@gmail.com>
2024-11-30 17:04:01 +0500

    changes

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -22,6 +22,10 @@ class TodoController
     public function show(Request $request, Response $response, $args)
     {
         $id = $args['id'];
+        if (!is_numeric($id)) {
+            $response->getBody()->write(json_encode(['success' => false, 'message' => "id must be number `$id is given` "]));
+            return $response->withStatus(405)->withHeader('Content-Type', 'application/json');
+        }
         $todo = Todo::find($id);
         if ($todo) {
             $todo = $response->getBody()->write(json_encode(['success', true, 'message' => 'Got resource successfully', 'data' => $todo]));
@@ -35,6 +39,10 @@ class TodoController
     public function delete(Request $request, Response $response, $args)
     {
         $id = $args['id'];
+        if (!is_numeric($id)) {
+            $response->getBody()->write(json_encode(['success' => false, 'message' => "id must be number `$id is given` "]));
+            return $response->withStatus(405)->withHeader('Content-Type', 'application/json');
+        }
         $todo = Todo::find($id);
 
         if ($todo) {
